09af240dc1a2383c175348ab9b5bd4d83190049f
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-01-11 20:00:50 -0500

    push some components

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Card (props) {
     const city = props.info;
     return (
-        <>
+        <div className='card'>
             <h1>{`${city.City}, ${city.State}`}</h1>
             <ul>
                 <li>{`State: ${city.State}`}</li>
                 <li>{`Location: (${city.Lat}, ${city.Long})`}</li>
-                <li>{`Population: Estimated:${city.EstimatedPopulation}`}</li>
+                <li>{`Population (estimated): ${city.EstimatedPopulation}`}</li>
                 <li>{`Total Wages: ${city.TotalWages}`}</li>
             </ul>
-        </>
+        </div>
     )
 } 
---
+++ src/components/Search.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card"
+
+export default function Search() {
+    const [cards, setCards] = useState("");
+    let searchText; 
+    useEffect(() => {
+        searchText = document.getElementById('zipSearch');
+        searchText.addEventListener('keyup', () => {
+            if (searchText.value.length !== 5 || !Number(searchText.value)) {
+                setCards(<h1>Not a zipcode</h1>);
+            } else {
+                fetch("https://ctp-zip-api.herokuapp.com/zip/" + searchText.value)
+                .then(res => res.json())
+                .then(data => {
+                    setCards(data.map((x) => <Card key={x.RecordNumber} info={x}/>));
+                })
+            }
+        })
+    }, []);
+    return (
+        <>
+            {cards}
+        </>
+    )
+}
