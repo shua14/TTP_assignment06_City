a240ebd535d73ff568d89a3d2fb77a273bb561bc
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-01-11 23:19:20 -0500

    removed unnecessary template strings

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,18 @@ import React from 'react'
 export default function Card (props) {
 
     const city = props.info
-
+    
     return (
         // simple card component - renders a div with the information, as shown in the example
         <div className='card'>
             <div className= "topLine">
-                <h2>&emsp;{`${city.LocationText}`}</h2>
+                <h2>{city.LocationText}</h2>
             </div>
             <ul>
-                <li>&nbsp;{`State: ${city.State}`}</li>
-                <li>&nbsp;{`Location: (${city.Lat}, ${city.Long})`}</li>
-                <li>&nbsp;{`Population (estimated): ${city.EstimatedPopulation ? city.EstimatedPopulation : "No data"}`}</li>
-                <li>&nbsp;{`Total Wages: ${city.TotalWages ? city.TotalWages : "No data"}`}</li>
+                <li>State: {city.State}</li>
+                <li>Location: ({city.Lat}, {city.Long})</li>
+                <li>Population (estimated): {city.EstimatedPopulation ? city.EstimatedPopulation : "No data"}</li>
+                <li>Total Wages: {city.TotalWages ? city.TotalWages : "No data"}</li>
             </ul>
         </div>
     )
